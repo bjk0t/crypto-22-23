ab48fcd29ab3e3d9b03ca3a9ec813f737b46c9b7
8BitRav3n <antheqes@gmail.com>
2023-01-25 16:17:15 +0300

    made it OOP

--- cp4/Poshyvak_Borovkov_CP4/RSA_crypto.py
+++ cp4/Poshyvak_Borovkov_CP4/RSA_crypto.py
@@ -4,118 +4,122 @@ import math as m
 PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
 
-def miller_test(p):
-    if p < 2:
-        return False
-    if p in PRIMES:
-        return True
-    if p % 2 == 0:
-        return False
+class RSA():
+    def __init__(self):
+        pass
 
-    d = p - 1
-    s = 0
-    while d % 2 == 0:
-        d //= 2
-        s += 1
-
-    for _ in range(18):
-        a = rand.randint(2, p - 2)
-        x = pow(a, d, p)
-        if x == 1 or x == p - 1:
-            continue
-        for _ in range(s - 1):
-            x = pow(x, 2, p)
-            if x == p - 1:
-                break
-        else:
+    @staticmethod
+    def miller_test(p):
+        if p < 2:
+            return False
+        if p in PRIMES:
+            return True
+        if p % 2 == 0:
             return False
 
-    return True
+        d = p - 1
+        s = 0
+        while d % 2 == 0:
+            d //= 2
+            s += 1
 
+        for _ in range(18):
+            a = rand.randint(2, p - 2)
+            x = pow(a, d, p)
+            if x == 1 or x == p - 1:
+                continue
+            for _ in range(s - 1):
+                x = pow(x, 2, p)
+                if x == p - 1:
+                    break
+            else:
+                return False
 
-def generate_prime(bits=256):
-    while True:
-        p = rand.getrandbits(bits)
-        if p % 2 != 0 and miller_test(p):
-            return p
-
-
-def generate_keys():
-    keys = []
-    while len(keys) < 4:
-        keys.append(generate_prime(256))
-        if len(keys) == 4 and keys[0] * keys[1] > keys[2] * keys[3]:
-            keys = []
-    return keys
-
-
-def extended_euclidean(a, b):
-    if b != 0:
-        gcd, x, y = extended_euclidean(b, a % b)
-        return (gcd, y, x - (a // b) * y)
-    return (a, 1, 0)
-
-
-def reverse(a, b):
-    gcd = m.gcd(a, b)
-    if gcd != 1:
-        return None
-    else:
-        gcd, x, y = extended_euclidean(b, a % b)
-        return y
-
-
-def generate_key_pair_rsa(p, q):
-    n = p * q
-    phi_n = (p - 1) * (q - 1)
-    e = rand.randrange(2, phi_n - 1)
-    while m.gcd(e, phi_n) != 1:
-        e = rand.randrange(2, phi_n - 1)
-    d = reverse(e, phi_n)
-    return {'e': e, 'n': n, 'd': d}
-
-
-#   #   #   #   #   #   #   #   #
-
-def encrypt(message, public_key, modulus):
-    return pow(message, public_key, modulus)
-
-
-def decrypt(ciphertext, private_key, modulus):
-    return pow(ciphertext, private_key, modulus)
-
-
-def sign(message, private_key, modulus):
-    return pow(message, private_key, modulus)
-
-
-def verify(message, signature, public_key, modulus):
-    if message == pow(signature, public_key, modulus):
         return True
-    return False
+
+    @staticmethod
+    def generate_prime(bits=256):
+        while True:
+            p = rand.getrandbits(bits)
+            if p % 2 != 0 and RSA.miller_test(p):
+                return p
+
+    @staticmethod
+    def generate_keys():
+        keys = []
+        while len(keys) < 4:
+            keys.append(RSA.generate_prime(256))
+            if len(keys) == 4 and keys[0] * keys[1] > keys[2] * keys[3]:
+                keys = []
+        return keys
+
+    @staticmethod
+    def extended_euclidean(a, b):
+        if b != 0:
+            gcd, x, y = RSA.extended_euclidean(b, a % b)
+            return (gcd, y, x - (a // b) * y)
+        return (a, 1, 0)
+
+    @staticmethod
+    def reverse(a, b):
+        gcd = m.gcd(a, b)
+        if gcd != 1:
+            return None
+        else:
+            gcd, x, y = RSA.extended_euclidean(b, a % b)
+            return y
+
+    @staticmethod
+    def generate_key_pair_rsa(p, q):
+        n = p * q
+        phi_n = (p - 1) * (q - 1)
+        e = rand.randrange(2, phi_n - 1)
+        while m.gcd(e, phi_n) != 1:
+            e = rand.randrange(2, phi_n - 1)
+        d = RSA.reverse(e, phi_n)
+        return {'e': e, 'n': n, 'd': d}
+
+    #   #   #   #   #   #   #   #   #
+
+    @staticmethod
+    def encrypt(message, public_key, modulus):
+        return pow(message, public_key, modulus)
+
+    @staticmethod
+    def decrypt(ciphertext, private_key, modulus):
+        return pow(ciphertext, private_key, modulus)
+
+    @staticmethod
+    def sign(message, private_key, modulus):
+        return pow(message, private_key, modulus)
+
+    @staticmethod
+    def verify(message, signature, public_key, modulus):
+        if message == pow(signature, public_key, modulus):
+            return True
+        return False
+
+    #   #   #   #   #   #   #   #   #
+
+    @staticmethod
+    def send_key(key, private_key, receiver_public_key, receiver_modulus, modulus):
+        encrypted_key = RSA.encrypt(key, receiver_public_key, receiver_modulus)
+        signature = RSA.sign(key, private_key, modulus)
+        encrypted_signature = RSA.encrypt(signature, receiver_public_key, receiver_modulus)
+        return encrypted_key, encrypted_signature
+
+    @staticmethod
+    def receive_key(encrypted_key, encrypted_signature, private_key, modulus, sender_pub_key, sender_modulus):
+        local_signature = RSA.decrypt(encrypted_signature, private_key, modulus)
+        local_key = RSA.decrypt(encrypted_key, private_key, modulus)
+
+        if RSA.verify(local_key, local_signature, sender_pub_key, sender_modulus):
+            print("[+] Bob got the message")
+            print(f"> Decrypted key: {local_key}\n")
+            return local_key
 
 
-#   #   #   #   #   #   #   #   #
-
-
-def send_key(key, private_key, receiver_public_key, receiver_modulus, modulus):
-    encrypted_key = encrypt(key, receiver_public_key, receiver_modulus)
-    signature = sign(key, private_key, modulus)
-    encrypted_signature = encrypt(signature, receiver_public_key, receiver_modulus)
-    return encrypted_key, encrypted_signature
-
-
-def receive_key(encrypted_key, encrypted_signature, private_key, modulus, sender_pub_key, sender_modulus):
-    local_signature = decrypt(encrypted_signature, private_key, modulus)
-    local_key = decrypt(encrypted_key, private_key, modulus)
-
-    if verify(local_key, local_signature, sender_pub_key, sender_modulus):
-        print("[+] Bob got the message")
-        print(f"> Decrypted key: {local_key}\n")
-        return local_key
-
-
-keys = generate_keys()
+keys = RSA.generate_keys()
 print(f"""
 keys:
 {str(keys[0])[:12:]}...
@@ -124,8 +128,8 @@ keys:
 {str(keys[3])[:12:]}...
 """)
 
-RSA_Alice = generate_key_pair_rsa(keys[0], keys[1])
-RSA_Bob = generate_key_pair_rsa(keys[2], keys[3])
+RSA_Alice = RSA.generate_key_pair_rsa(keys[0], keys[1])
+RSA_Bob = RSA.generate_key_pair_rsa(keys[2], keys[3])
 
 print("#            INFO")
 print(f"""#    _Alice
@@ -155,18 +159,18 @@ key = rand.randint(1, RSA_Alice['n'])
 print(f"> Generated message: {message}")
 
 # Alice making signature (signature1) and send it to Bob
-_key, _signature = send_key(key, RSA_Alice['d'], RSA_Bob['e'], RSA_Bob['n'], RSA_Alice['n'])
-encrypted = encrypt(message, RSA_Alice['e'], RSA_Alice['n'])
+_key, _signature = RSA.send_key(key, RSA_Alice['d'], RSA_Bob['e'], RSA_Bob['n'], RSA_Alice['n'])
+encrypted = RSA.encrypt(message, RSA_Alice['e'], RSA_Alice['n'])
 print("> Encrypted message: ", encrypted, end='\n')
 # sending...
 
 # Bob receiving message
-_key = receive_key(_key, _signature, RSA_Bob['d'], RSA_Bob['n'], RSA_Alice['e'], RSA_Alice['n'])
+_key = RSA.receive_key(_key, _signature, RSA_Bob['d'], RSA_Bob['n'], RSA_Alice['e'], RSA_Alice['n'])
 
 if _key:
     print("[+] Bob have received the message.")
     # Bob decrypting message with his private_key
-    decrypted = decrypt(encrypted, RSA_Alice['d'], RSA_Alice['n'])
+    decrypted = RSA.decrypt(encrypted, RSA_Alice['d'], RSA_Alice['n'])
     if decrypted:
         print("[+] Bob have decrypted the message.")
         print(f"> Decrypted message: {decrypted}\n")
