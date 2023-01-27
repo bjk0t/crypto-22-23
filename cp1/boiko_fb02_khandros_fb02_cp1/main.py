d8c915b9e0197c72fb3a5ea525460a2699a2bdff
bjk0t <87545033+bjk0t@users.noreply.github.com>
2023-01-27 14:11:11 +0200

    fixed bug with bigrams

--- cp1/boiko_fb02_khandros_fb02_cp1/main.py
+++ cp1/boiko_fb02_khandros_fb02_cp1/main.py
@@ -1,109 +1,94 @@
+import math
 import re
-from math import log2
-import pandas as pd
+
+alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя"
+alphabet_sp = "абвгдежзийклмнопрстуфхцчшщыьэюя "
+
+# spaces -- bool; true - remove spaces in text
+def clear_file(filename, spaces=True):
+    file = open(filename, "r", encoding='utf-8')
+    text = file.read()
+    text = text.replace("\n", " ").replace("\r", "").lower().replace("ё", "е").replace("ъ", "ь")
+    text = re.sub(r"[^а-я ]", "", text)
+    if spaces:
+        text = text.replace(" ", "");
+    return text
+
+def freq_lett(text):
+    letters = {}
+    for i in text:
+        try:
+            letters[i] += 1
+        except:
+            letters[i] = 1
+    freq = {}
+    for i in letters:
+        freq[i] = letters[i]/len(text)
+    return freq
+
+# crossed -- bool; true -- using crossing bigrams
+def freq_bi(text, crossed):
+    birgams = {}
+    len_t = len(text) if crossed else len(text)*2
+    for i in range(0,len(text)-1, 2 if crossed else 1):
+        try:
+            birgams[text[i]+ text[i+1]] += 1
+        except:
+            birgams[text[i]+ text[i+1]] = 1
+    freq_bigrams = {}
+    for i in birgams:
+        freq_bigrams[i] = birgams[i]/len_t
+    return freq_bigrams
+
+# len_t -- len of text 
+def entropy(freq):
+    entropy = 0
+    for i in freq:
+        entropy -= freq[i] * math.log2(freq[i])
+    return entropy
 
 
-with open('data.txt', 'r', encoding='utf-8') as file:
-    data_str = file.read().replace('\n', ' ')
-data_str.lower()
-pattern1 = '[^а-я ]+'  #паттерн для очистки від зайвих символів
-pattern2 = '[^а-я]+'  #паттерн для очистки від пробілів
-data_str = re.sub(pattern1, '', data_str)
-data_str_spaces = ' '.join(data_str.split())  #чистий текст з пробілами
-data_str_no_spaces = re.sub(pattern2, '', data_str)  #чистий текст без пробілів
-alph_spaces = 'абвгдеёэжзиыйклмнопрстуфхцчшщъьюя '
-alph_no_spaces = 'абвгдеёэжзиыйклмнопрстуфхцчшщъьюя'
+filename = "data.txt"
+text_sp = clear_file(filename, spaces=False)
+text = clear_file(filename)
+
+H1_no_sp = entropy(freq_lett(text))
+R1_no_sp = 1 - H1_no_sp/math.log2(31)
+print("Ентропія H1 без пробілів",H1_no_sp)
+print("Надлишковість R1 без пробілів", R1_no_sp)
+
+H1_sp = entropy(freq_lett(text))
+R1_sp = 1 - H1_sp/math.log2(32)
+print("Ентропія H1 з пробілами",H1_sp)
+print("Надлишковість R1 з пробілами", R1_sp)
 
 
+freq_lett_no_sp = freq_lett(text_sp)
+print("Ентропія H1 без пробілів",entropy(freq_lett_no_sp))
 
-def frequency_letters(string):
-    text_len = len(string)
-    freq_chars = {}
-    for i in string:    #отримуєм словник з абсолютними частотами
-        if i in freq_chars:
-            freq_chars[i] += 1
-        else:
-            freq_chars[i] = 1
-    rel_freq_chars = {}
-    for letter in freq_chars.keys():    #отримуєм словник з відносними частотами
-        rel_freq_chars[letter] = round((freq_chars[letter]) / text_len, 4)
-    return rel_freq_chars
-    # analitics = Counter(strings)
-    # analitics = {key: value for key, value in analitics.items() if key.isalpha()}
-    # return analitics
+H2_no_sp_cross = entropy(freq_bi(text, 1))
+R2_no_sp_cross = 1 - H2_no_sp_cross/math.log2(31)
+print("Ентропія H2 без пробілів, біграми пересікаються", H2_no_sp_cross)
+print("Надлишковість R2 без пробілів, біграми пересікаються", R2_no_sp_cross)
 
+H2_sp_cross = entropy(freq_bi(text_sp, 1))
+R2_sp_cross = 1 - H2_sp_cross/math.log2(32)
+print("Ентропія H2 з пробілами, біграми пересікаються", H2_sp_cross)
+print("Надлишковість R2 з пробілами, біграми пересікаються", R2_sp_cross)
 
-def frequency_bigrams(string):
-    bi1 = re.findall('..', string)
-    cl_data_str_r = string[1:] + string[0]
-    #bi2 = re.findall('..', cl_data_str_r)  #враховуєм перехресні біграми
-    bi2 = []  #не враховуєм перехресні біграми
-    bigrams = bi1 + bi2
-    bi_count = len(string)
-    freq_bi = {}
-    for i in bigrams:   #отримуєм словник з абсолютними частотами
-        if i in freq_bi:
-            freq_bi[i] += 1
-        else:
-            freq_bi[i] = 1
-    rel_freq_bi = {}
-    for pair in freq_bi.keys():   #отримуєм словник з відносними частотами
-         rel_freq_bi[pair] = round((freq_bi[pair]) / bi_count, 10)
-    return rel_freq_bi
+H2_no_sp_no_cross = entropy(freq_bi(text, 0))
+R2_no_sp_no_cross = 1 - H2_no_sp_no_cross/math.log2(31)
+print("Ентропія H2 без пробілів, біграми не пересікаються", H2_no_sp_no_cross)
+print("Надлишковість R2 без пробілів, біграми не пересікаються", R2_no_sp_no_cross)
 
+H2_sp_no_cross = entropy(freq_bi(text_sp, 0))
+R2_sp_no_cross = 1 - H2_sp_no_cross/math.log2(32)
+print("Ентропія H2 з пробілами, біграми пересікаються", H2_sp_no_cross)
+print("Надлишковість R2 з пробілами, біграми пересікаються", R2_sp_no_cross)
 
-def h1(string):
-    h1 = 0
-    vals = frequency_letters(string).values()
-    for i in vals:
-        h1 += - i * log2(i)
-    return h1
-
-
-def h2(string):
-    h2 = 0
-    vals = list(frequency_bigrams(string).values())
-    for i in vals:
-        h2 += - i * log2(i)
-    return h2
-
-
-def r_(string, h, alphabet):
-    h0 = log2(len(alphabet))
-    r = 0
-    if h == 1:
-        h = h1(string)
-        r = 1 - (h / h0)
-    elif h == 2:
-        h = h2(string)
-        r = 1 - (h / h0)
-    return r
-
-
-# fl_ws = frequency_letters(data_str_spaces)  #freqency letters - text with spaces
-# fl_ns = frequency_letters(data_str_no_spaces)  #frequency letters - text without spaces
-# bi_x_sp = frequency_bigrams(data_str_spaces)  #frequency crossed bigrams - text with spaces
-# bi_x_nosp = frequency_bigrams(data_str_no_spaces) #frequency crossed bigrams - text without spaces
-# bi_sp = frequency_bigrams(data_str_spaces)
-# bi_nosp = frequency_bigrams(data_str_no_spaces)
-# df = pd.DataFrame(data=bi_nosp, index=['bi freq without spaces'])
-# df = (df.T)
-# df.to_excel('df6.xlsx')
-# h1_sp = h1(data_str_spaces)
-# h1_nosp = h1(data_str_no_spaces)
-# h2_x_sp = h2(data_str_spaces)
-# h2_x_nosp = h2(data_str_no_spaces)
-# h2_sp = h2(data_str_spaces)
-# h2_nosp = h2(data_str_no_spaces)
-# print(f'Н1 with spaces: {h1_sp}')
-# print(f'H1 without spaces {h1_nosp}')
-# print(f'h2 x with spaces: {h2_sp}')
-# print(f'h2 x without spaces: {h2_nosp}')
-
-
-# r1 = r_(data_str_spaces, 1, alph_spaces)
-# r1_nosp = r_(data_str_no_spaces, 1, alph_no_spaces)
-# print(r1, r1_nosp)
-# r2 = r_(data_str_spaces, 2, alph_spaces)
-# r2_nosp = r_(data_str_no_spaces, 2, alph_no_spaces)
-# print(r2, r2_nosp)
+print("1.752 < H10 < 2.356")
+print( (1-(1.752/math.log2(32))), "> R >", (1-(2.356/math.log2(32))))
+print("1.784 < H20 < 2.492")
+print( (1-(1.784/math.log2(32))), "> R >", (1-(2.492/math.log2(32))))
+print("1.437 < H30 < 2.18")
+print( (1-(1.437/math.log2(32))), "> R >", (1-(2.18/math.log2(32))))
